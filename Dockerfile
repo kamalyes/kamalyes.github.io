bac8e0dc6ad2b1c63a360df55ed3707ab917f1d5
kamalyes <501893067@qq.com>
2023-02-18 13:07:51 +0800

    :fire: feat:增加几个docker实例

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# 拉取nodejs 16.19.1版本镜像  其它版本访：https://hub.docker.com/_/node/tags?page=1
+FROM node:16.19.1-alpine3.17 AS builder
+
+# 制定工作目录并执行yarn install
+WORKDIR /opt/web
+COPY package.json yarn.lock ./
+RUN yarn install
+
+# 设置nodemodules环境变量
+ENV PATH="./node_modules/.bin:$PATH"
+
+# 复制本地路径下所有文件并执行build
+COPY . ./
+
+RUN pwd
+RUN yarn run docs:build
+RUN yarn run fileDisplay
+
+# 拉取nginx1.17版本镜像 https://hub.docker.com/_/nginx/tags?page=1
+FROM nginx:1.17-alpine
+
+# 安装一些常用插件
+RUN apk --no-cache add curl vim wget
+RUN curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o envsubst && \
+    chmod +x envsubst && \
+    mv envsubst /usr/local/bin
+
+# 复制nginx.config文件
+COPY ./nginx.config /etc/nginx/nginx.template
+
+# 使用envsubst替换nginx.config${..}变量
+CMD ["/bin/sh", "-c", "envsubst < /etc/nginx/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"]
+
+# 复制上一个（builder）镜像至当前镜像内
+COPY --from=builder /opt/web/docs/.vuepress/dist /usr/share/nginx/html
---
+++ utils/fileDisplay.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '..', "docs/.vuepress/dist"); // 要批量修改的文件路径
+
+//调用文件遍历方法
+fileDisplay(filePath);
+
+/**
+ * 文件遍历方法
+ * @param filePath 需要遍历的文件路径
+ */
+function fileDisplay(filePath){
+    //根据文件路径读取文件，返回文件列表
+    fs.readdir(filePath,function(err,files){
+        if(err){
+            console.warn(err)
+        }else{
+            //遍历读取到的文件列表
+            files.forEach(function(filename){
+                //获取当前文件的绝对路径
+                const filedir = path.join(filePath,filename);
+                //根据文件路径获取文件信息，返回一个fs.Stats对象
+                fs.stat(filedir,function(eror,stats){
+                    if(eror){
+                        console.warn('获取文件stats失败');
+                    }else{
+                        const isFile = stats.isFile();//是文件
+                        const isDir = stats.isDirectory();//是文件夹
+                        if(isFile){
+                            console.log(`获取到dist文件：${filedir}`);
+                        }
+                        if(isDir){
+                            fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
+                        }
+                    }
+                })
+            });
+        }
+    });
+}
